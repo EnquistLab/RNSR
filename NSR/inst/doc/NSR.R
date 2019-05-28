5113b132c9774bcc5fbd837b1db4c43f71f1f771
bmaitner <bmaitner@gmail.com>
2019-05-28 11:28:42 -0700

    updated vignette to reflect switch from 7 input columns to 6

--- NSR/inst/doc/NSR.R
+++ NSR/inst/doc/NSR.R
@@ -23,10 +23,10 @@ library(BIEN)
 
 pima <- BIEN_occurrence_county(country = "United States",state = "Arizona",county = "Pima", all.taxonomy = T,natives.only = F,native.status = T,cultivated = T,political.boundaries = T, limit = 100)
 
-#The NSRS function expects 7 columns as input, however only the fields "species" and "country" need to be populated.
-#If you ever forget, you can use the function NSRS_template as a quick lookup, or to populate.
+#The NSR function expects 6 columns as input, however only the fields "species" and "country" need to be populated.
+#If you ever forget, you can use the function NSR_template as a quick lookup, or to populate.
 
-pima_query<-NSRS_template(nrow = nrow(pima))
+pima_query<-NSR_template(nrow = nrow(pima))
 
 pima_query$family <- pima$scrubbed_family
 pima_query$species <- pima$scrubbed_species_binomial
