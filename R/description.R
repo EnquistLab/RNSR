d5c7109c2761aec42ff8097faa9f49c3da0f9cbc
Brian <bmaitner@gmail.com>
2024-10-09 15:22:32 -0500

    documentation

--- R/description.R
+++ R/description.R
@@ -1,12 +1,13 @@
 #' NSR: Tools for accessing the Native Species Resolver (NSR).
 #'
-#' @description The Native Species Resolver (NSR) R package provides access to the NSR as well as other associated tools.
+#' @description The Native Species Resolver (NSR) R package provides access to the NSR <https://github.com/ojalaquellueva/nsr> as well as other associated tools. For more information on the NSR, see <https://github.com/ojalaquellueva/nsr>.
 #' 
 #' @section Getting started:
 #' Type vignette("NSR") to view the vignette, which contains useful information on the NSR package.
 #' 
-#' @references Enquist, B.J., Sandel, B., Boyle, B., Donoghue II, J.C., Regetz, J., Svenning, J.C., McGill, B.J., Peet, R.K., Jorgensen, P.M., Condit, R., Thiers, B., Schildhauer, M., Smith, S.A., Hinchliff, C.E., Wiser, S.K., Violle, C., Simova, I., Spencer, N., Dolins, S., Morueta-Holme, N., Marcuse-Kubitza, A., Kraft, N.J.B., Ott, J.E., Andelman, S., ter Steege, H., Phillips, O., Sloat, L.L., Narro, M.L., Casler, N., Guaderama, D.,  Merow, C., Maitner, B.S. (in prep) A general signature of taxonomic and phylogenetic diversity across the Land Plants of the New World.
+#' @references Maitner, B.S., Boyle, B., Casler, N., Condit, R., Donoghue, J., Durán, S.M., Guaderrama, D., Hinchliff, C.E., Jørgensen, P.M., Kraft, N.J. and McGill, B., 2018. The bien r package: A tool to access the Botanical Information and Ecology Network (BIEN) database. Methods in Ecology and Evolution, 9(2), pp.373-379.
 #' 
 #' @docType package
 #' @name NSR
 #' @aliases NSR-package
+
--- cran-comments.md
+++ cran-comments.md
@@ -11,6 +11,6 @@
 
 0 errors √ | 0 warnings √ | 0 notes √
 
-* This is a new release.
 
 ## Notes
+* win-builder returns two notes: (1) "CRAN-pack does not know about .github", which is not returned otherwise (.github is needed for rhub and is in the .Rbuildignore); and (2) "This is a new release.""
